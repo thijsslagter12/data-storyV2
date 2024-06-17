1dcb5d761207bcc1b7120105c92a25874ea25061
tomdelange <tomdelange03@icloud.com>
2024-06-17 22:29:30 +0200

    merged aangepast en map2 toegevoegd

--- docs/databases/mapchart.py
+++ docs/databases/mapchart.py
@@ -2,16 +2,35 @@ import plotly.express as px
 import pandas as pd
 
 # Laad de dataset
-data = pd.read_csv('merged_dataset2.csv')
+data = pd.read_csv('merged_dataset2_updated.csv')
 
-# Maak een choropleth-kaart
-fig = px.choropleth(
+# Voorbeeld structuur van de oorspronkelijke dataset:
+# Country, iso_a3, Happiness Score 2019, Happiness Score 2020, Happiness Score 2021
+# Afghanistan, AFG, 3.2, 3.4, 3.5
+
+# Zet de dataset om naar lange vorm
+data_long = pd.melt(
     data,
+    id_vars=['Country', 'iso_a3'],
+    value_vars=['Happiness Score 2020','Happiness Score 2019', 'Happiness Score 2018', 'Happiness Score 2017','Happiness Score 2016',
+                'Happiness Score 2015'],
+    var_name='Year',
+    value_name='Happiness Score'
+)
+
+# Extract jaar uit de variabelenaam
+data_long['Year'] = data_long['Year'].str.extract('(\d+)').astype(int)
+
+
+# Maak een choropleth-kaart met een slider voor jaren
+fig = px.choropleth(
+    data_long,
     locations='iso_a3',  # Gebruik de kolom met ISO 3166-1 alpha-3 landcodes
-    color='happy Score 2019',       # Gebruik de 'Score' kolom om kleuren te bepalen
-    hover_name='Country',# Toon de landnamen bij hover
+    color='Happiness Score',  # Gebruik de 'Happiness Score' kolom om kleuren te bepalen
+    hover_name='Country',  # Toon de landnamen bij hover
+    animation_frame='Year',  # Gebruik de 'Year' kolom voor de animatie
     color_continuous_scale=px.colors.sequential.Plasma,  # Kleurschaal
-    title='Happiness Around the World'
+    title='Happiness Around the World Over Years'
 )
 
 fig.show()
