f6f67a43663f78e0797dd849150d01685c5f2826
thijsslagter12 <thijs.slagter@student.uva.nl>
2024-06-18 20:06:34 +0200

    aangepast tekstje plot

--- docs/databases/directGDPhappinesplot.py
+++ docs/databases/directGDPhappinesplot.py
@@ -3,19 +3,19 @@ import plotly.express as px
 import numpy as np
 
 # Load the dataset
-file_path = '2019.csv'
+file_path = 'merged_dataset2_updated.csv'
 data = pd.read_csv(file_path)
 
 # Calculate the correlation between Ladder score and Logged GDP per capita
-correlation = data['Score'].corr(data['GDP per capita'])
+correlation = data['Happiness Score 2020'].corr(data['Logged GDP per capita'])
 
 # Calculate the mean GDP per capita and happiness score
-mean_gdp = np.mean(data['GDP per capita'])
-mean_score = np.mean(data['Score'])
+mean_gdp = np.mean(data['Logged GDP per capita'])
+mean_score = np.mean(data['Happiness Score 2020'])
 
 # Create a scatter plot with tooltips and a mean line
-fig = px.scatter(data, x='GDP per capita', y='Score', hover_name='Country or region',
-                 title=f'<b>Correlation between GDP per capita and Happiness Score</b> (C: {correlation:.2f})',
+fig = px.scatter(data, x='Logged GDP per capita', y='Happiness Score 2020', hover_name='Country',
+                 title=f'<b>Correlation between GDP per capita and Happiness Score</b> (Correlation: {correlation:.2f})',
                  trendline='ols')  # Ordinary Least Squares (OLS) trendline for mean
 
 # Update layout for aesthetics
